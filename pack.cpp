e548b6b60be379f57f537571dd885ef17ad23d15
kach3242 <kach3242@hanmail.net>
2019-03-29 02:30:08 +0900

    modefied pack.*

--- pack.cpp
+++ pack.cpp
@@ -1,47 +1,44 @@
 #include <stdio.h>
+#include <stdint.h>
+#include <string.h>
 #include <arpa/inet.h>
 #include "pack.h"
 
 void pack(const unsigned char* data){
-    unsigned short ethertype[] = {*(data+12), *(data+13)};
-    unsigned short* ethtype = (unsigned short*)ethertype;
-    unsigned short iptype = *(data+23);
-    *ethtype=ntohs(*ethtype);
-    unsigned int iplen = (*(data+14) - 0x0040)*4;
-    unsigned int Sport = *(data+iplen+14)*256 + *(data+iplen+15);
-    unsigned int Dport = *(data+iplen+16)*256 + *(data+iplen+17);
-    unsigned int tcplen = (*(data+46)/16)*4;
-    if(*ethertype == 0x0800 && iptype == 6 && Dport == 80){
-        printf("Smac : ");
-        for(int i=6; i<12; i++){
-            printf("%02x:", *(data+i));
-            if(i==11){
-                printf("\b \nDmac : ");
-                
-                i = -1;
-            }
-            if(i==5){
-                printf("\b \n");
-                break;
+    struct eth_header *eth = (struct eth_header *)data;
+    struct ip_header *ip = (struct ip_header *)(data+eth_size);
+    uint8_t ipv4_len = (ip->ipv4_len & 0x0F)<<2;
+    struct tcp_header *tcp = (struct tcp_header *)(data+eth_size+ipv4_len);
+    uint8_t tcp_len = (tcp->hlen & 0xF0)>>2;
+    char *http_data = (char *)(data+eth_size+ipv4_len+tcp_len);
+    if(ntohs(eth->eth_type) == ipv4 && ip->pid == 6 && (ntohs(tcp->sport) == 80 || ntohs(tcp->dport) == 80)){
+        printf("smac : ");
+        for(int i=0; i<6; i++){
+            printf("%02x:",eth->smac[i]);
+        }
+        printf("\b \n");
+        printf("dmac : ");
+        for(int i=0; i<6; i++){
+            printf("%02x:",eth->dmac[i]);
+        }
+        printf("\b \n");
+        printf("eth_type : %04x\n",ntohs(eth->eth_type));
+        printf("sip : %s\n",inet_ntoa(*(struct in_addr *)&ip->sip));
+        printf("dip : %s\n",inet_ntoa(*(struct in_addr *)&ip->dip));
+        printf("ip_len : %d\n",ipv4_len);
+        printf("pid : %d\n", ip->pid);
+        printf("sport : %d\n",ntohs(tcp->sport));
+        printf("dport : %d\n",ntohs(tcp->dport));
+        printf("tcp_len : %d\n",tcp_len);
+        printf("http_data_len : %d\n",strlen(http_data));
+        if(strlen(http_data)>16){
+            for (int i=0; i<16; i++) {
+                printf("%c", http_data[i]);
             }
         }
-        printf("SIP  : ");
-        for (int i=26; i<34; i++) {
-            printf("%d.", *(data+i));
-            if(i==29){
-                printf("\b \nDIP  : ");
-            }
-            else if(i==33){
-                printf("\b \n");
-            }
-        }
-        printf("Sport  [%d]\n", Sport);
-        printf("Dport  [%d]\n", Dport);
-        printf("HTTP DATA : ");
-        for (int i=0; i<16; i++){ 
-            printf("%c", *(data+iplen+tcplen+14+i));
+        else {
+            printf("%s",http_data);
         }
         printf("\n\n");
     }
 }
-
--- pack.h
+++ pack.h
@@ -1,4 +1,39 @@
 #pragma once
+#include <stdint.h>
+
+#define eth_size 14
+#define ipv4 0x0800
+
+struct eth_header{
+    uint8_t dmac[6];
+    uint8_t smac[6];
+    uint16_t eth_type;
+};
+
+struct ip_header{
+    uint8_t ipv4_len;
+    uint8_t tos;
+    uint16_t packet_len;
+    uint8_t id;
+    uint16_t flag;
+    uint8_t ttl;
+    uint8_t pid;
+    uint16_t checksum;
+    uint32_t sip;
+    uint32_t dip;
+};
+
+struct tcp_header{
+    uint16_t sport;
+    uint16_t dport;
+    uint32_t seqnum;
+    uint32_t acknum;
+    uint8_t hlen;
+    uint8_t flag;
+    uint16_t wsize;
+    uint16_t checksum;
+    uint16_t urg_pointer;
+};
 
 void pack(const unsigned char* data);
 
